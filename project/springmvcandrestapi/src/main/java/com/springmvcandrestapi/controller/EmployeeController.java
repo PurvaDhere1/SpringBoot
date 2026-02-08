6c41f51ddcd6611810cc884c98152e10ad7f5a43
PurvaSDhere <purvasdhere@gmail.com>
2026-02-08 20:11:38 +0530

    employee controller

--- project/springmvcandrestapi/src/main/java/com/springmvcandrestapi/controller/EmployeeController.java
+++ project/springmvcandrestapi/src/main/java/com/springmvcandrestapi/controller/EmployeeController.java
@@ -1,5 +1,26 @@
 package com.springmvcandrestapi.controller;
 
+import com.springmvcandrestapi.dto.EmployeeDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
 public class EmployeeController {
+
+    @GetMapping(path="/secretmessage")
+    public String getMessage(){
+       return "Here is your messagessss";
+    }
+
+    @GetMapping(path="/employee/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
+        return new EmployeeDTO(employeeId, "Purva", 40000.00, LocalDate.of(2023,01,11), true );
+    }
+
+    @PostMapping(path = "/create/employee")
+    public EmployeeDTO createEmployeeById(@RequestBody EmployeeDTO req){
+       return req;
+   }
 
 }
--- project/springmvcandrestapi/src/main/java/com/springmvcandrestapi/entities/EmployeeEntity.java
+++ project/springmvcandrestapi/src/main/java/com/springmvcandrestapi/entities/EmployeeEntity.java
@@ -2,9 +2,9 @@ package com.springmvcandrestapi.entities;
 
 import java.time.LocalDate;
 
-public class EmployeeEntity {
+/*public class EmployeeEntity {
     Long id;
     String name;
     Double salary;
     LocalDate dateOfJoining;
-}
+}*/
