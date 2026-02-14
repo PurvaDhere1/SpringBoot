7e0e287f1f01adae026bbe91c9033c725d6ee05d
PurvaSDhere <purvasdhere@gmail.com>
2026-02-14 21:06:48 +0530

    put and delete method

---
+++ project/springmvcandrestapi/src/main/java/com/springmvcandrestapi/SpringmvcandrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.springmvcandrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcandrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcandrestapiApplication.class, args);
+	}
+
+}
---
+++ project/springmvcandrestapi/src/test/java/com/springmvcandrestapi/SpringmvcandrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvcandrestapi;
+
+/*import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcandrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
